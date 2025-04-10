0906ad01d44a393ca4edd43e8270308d3aa3bd1c
Gloire28 <gloiredakla28@gmail.com>
2025-04-10 02:55:22 +0000

    Synchronisation complète de la version locale de l'application

--- generate_passwords.py
+++
@@ -1,8 +0,0 @@
-# generate_passwords.py
-from werkzeug.security import generate_password_hash
-
-users = ['Juste', 'Kevine', 'Joseph', 'Gamal', 'Eme', 'Spero', 'Gloir']
-for user in users:
-    password = "password123" 
-    hashed_password = generate_password_hash(password)
-    print(f"{user}: {hashed_password}")
